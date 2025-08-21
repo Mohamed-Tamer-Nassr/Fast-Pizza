8d8d3f32f6201dbd83b59234a7d84d6c70d376a2
Mohamed-Tamer-Nassr <mohamed.tamer.mtn@gmail.com>
2025-08-21 21:39:43 +0300

    init --version@0.6

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <p className="text-sm font-semibold">Mohamed</p>;
+  return <p className="hidden text-sm font-semibold md:block">Mohamed</p>;
 }
 
 export default Username;
