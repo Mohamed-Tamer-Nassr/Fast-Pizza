be6ca88cc0d12a80b4555925bbacbc1af5b2294a
Mohamed-Tamer-Nassr <mohamed.tamer.mtn@gmail.com>
2025-08-21 03:26:33 +0300

    init --version@0.3

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,38 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Cart from "./features/cart/Cart";
 // The first of second step of fetching data with react router
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 import Home from "./ui/Home";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
       {
         path: "/menu",
         element: <Menu />,
+        errorElement: <Error />,
         // the second of the seconde step of fetching data with react router
         loader: menuLoader,
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/:orderId", element: <Order /> },
-      { path: "/order/new", element: <CreateOrder /> },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        errorElement: <Error />,
+        loader: orderLoader,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
     ],
   },
 ]);
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,11 +1,18 @@
 import { useLoaderData } from "react-router-dom";
 import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 
 function Menu() {
   // the third and the last Step for fetching data using react router
   const menu = useLoaderData();
   console.log(menu);
-  return <h1>Menu</h1>;
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 // The First Step for fetching data with react router
 export async function loader() {
