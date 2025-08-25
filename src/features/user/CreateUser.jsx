65f4108d69b132dc87c02b6be8d2868e1b1c858d
Mohamed-Tamer-Nassr <mohamed.tamer.mtn@gmail.com>
2025-08-25 21:15:31 +0300

    init --version@1.0

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import Button from "../../ui/Button";
+import { updateName } from "./userSlice";
 
 function CreateUser() {
   const [username, setUsername] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!username) return null;
+    if (username.length > 12) {
+      alert("Name is too long ,Try a nickname or Your first name");
+      return;
+    }
+    dispatch(updateName(username));
+    navigate("/menu");
   }
 
   return (
@@ -19,7 +31,7 @@ function CreateUser() {
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        className="input mb-8 w-72 uppercase"
+        className="input mb-8 w-72 text-center uppercase"
       />
 
       {username !== "" && (
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,11 @@
+import { useSelector } from "react-redux";
+
 function Username() {
+  const userName = useSelector((state) => state.user.userName);
+  if (!userName) return null;
   return (
     <p className="hidden text-sm font-semibold tracking-wider md:block md:text-2xl">
-      Mohamed
+      {userName}
     </p>
   );
 }
