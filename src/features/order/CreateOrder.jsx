83d2e0507c47bdde92574533414d90dfeed0c6b8
Mohamed-Tamer-Nassr <mohamed.tamer.mtn@gmail.com>
2025-08-28 04:15:27 +0300

    init --version@1.2

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,45 +2,23 @@ import { useSelector } from "react-redux";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
-
+import { getCart } from "../cart/cartSlice";
+import EmptyCart from "../cart/EmptyCart";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str,
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "vegetables",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
   const userName = useSelector((state) => state.user.userName);
 
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
   const formData = useActionData();
+  if (!cart.length) return <EmptyCart />;
   return (
     <div className="px-4">
       <h2 className="mb-8 text-xl font-semibold md:text-2xl">
